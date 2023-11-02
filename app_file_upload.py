01ae6bd95955b2baf28596a33eb033490e0d8b5b
Muriel Gordon <murielgordon6@gmail.com>
2023-11-01 17:47:19 -0700

    removed trailing e line 7 why do we even have that lever

--- app_file_upload.py
+++ app_file_upload.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 UPLOAD_FOLDER = 'static/uploads'
 
-app = Flask(__name__)e
+app = Flask(__name__)
 app.secret_key = 'secret key'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1280 * 1280
